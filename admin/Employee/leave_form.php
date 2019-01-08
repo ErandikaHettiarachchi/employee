63ec9aa8a8284212914bd9122b049a6e63d485c2
Erandika Harshani Hettiarachchi <38687522+ErandikaHettiarachchi@users.noreply.github.com>
2019-01-08 18:52:26 +0530

    leave form

--- admin/Employee/leave_form.php
+++ admin/Employee/leave_form.php
@@ -39,53 +39,62 @@
                     <form method="POST" action="leave_form.php">
                         <div class="box-body">
                             <div class="row">
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label>From:</label>
-                                        <input name = 'startdate' style="width:200px" type="date" class="form-control" id="startdate" placeholder=" " required>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label>To:</label>
-                                        <input name = 'enddate' style="width:200px" type="date" class="form-control" id="enddate" placeholder="" required>
-                                    </div>
-                                    
-                                </div>
                                 <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Reason:</label>
-                                        <textarea name='reason' style="width:450px" class="form-control" id="messageTxt"  placeholder="" required></textarea>
+                                    <div class=row>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label>From:</label>
+                                                <input name = 'startdate' style="width:200px" type="date" class="form-control" id="startdate" placeholder=" " required>
+                                            </div>
+                                        </div> 
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label>To:</label>
+                                                <input name = 'enddate' style="width:200px" type="date" class="form-control" id="enddate" placeholder="" required>
+                                            </div>    
+                                        </div>
                                     </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label>From:</label>
+                                                        <input  name = 'starttime' type="time" value="08:00"  min="8:00 AM" max="05:00 PM" class="form-control" style="width:200px"id="starttime" required>
+                                                        <!-- <input name = 'starttime' value="08:00 AM" min="8:00 AM" max="05:00 PM" style="width:200px" type="time" class="form-control" id="starttime"  required> -->
+                                                    </div> 
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label>To:</label>
+                                                        <input name = 'endtime' type="time" value="05:00"  class="form-control" style="width:200px" id="enddate" required>
+                                                        <!-- <input name = 'endtime' value="05:00 PM" min="8:00 AM" max="05:00 PM" style="width:200px" type="time" class="form-control" id="enddate" placeholder="" required> -->
+                                                    </div> 
+                                                </div>
+                                            </div>
+                                </div>
+                                <div class="row-md-6">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Reason:</label>
+                                            <textarea name='reason' style="width:450px" class="form-control" id="messageTxt"  placeholder="" required></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <button style="width:150px" name="leavBTN" type="post" class="btn btn-primary">Submit</button>
+                                        </div>
+                                    </div>  
                                 </div>
                             </div>
-                                <!--                                    type="datetime-local"-->
-                           
-                            <div class="row">
-                                <div class="col-md-9">
-
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <button style="width:150px" name="leavBTN" type="post" class="btn btn-primary">Submit</button>
-
-                                    </div>
-                                </div>
-                            </div>
-                            
-                    
                         </div>
-
                         <div class="box-footer">
-                                        
                         </div>
                     </form>
-                </div><!-- /.box -->
-            <!-- </div> -->
-        <!-- </div> -->
+                </div>
     </section>
 </section>
+
+
+
 
 <script>
     function checktime(){
@@ -111,4 +120,5 @@
             document.getElementById('aprlbl').innerHTML="ddd";
         }
     }
-</script>
+</script> -->
+
