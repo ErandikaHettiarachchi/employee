ea6005d4a04c776a3340cc9be7e99db6c51edf9d
Dinuka Kasun Medis <dinuka-kasun-medis@users.noreply.github.com>
2019-01-09 07:46:49 +0530

    test2

--- admin/Employee/employeeComman/head.php
+++ admin/Employee/employeeComman/head.php
@@ -1,66 +1,70 @@
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Motor | Boss</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="../bower_components/Ionicons/css/ionicons.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
-    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
-          page. However, you can choose any other skin. Make sure you
-          apply the skin class to the body tag so the changes take effect. -->
-    <link rel="stylesheet" href="../dist/css/skins/skin-blue.min.css">
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Motor | Boss</title>
+      <!-- Tell the browser to be responsive to screen width -->
+      <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+      <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
+      <!-- Font Awesome -->
+      <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
+      <!-- Ionicons -->
+      <link rel="stylesheet" href="../bower_components/Ionicons/css/ionicons.min.css">
+      <!-- Theme style -->
+      <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
+      <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+            page. However, you can choose any other skin. Make sure you
+            apply the skin class to the body tag so the changes take effect. -->
+      <link rel="stylesheet" href="../dist/css/skins/skin-blue.min.css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+      <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
 
-    <!-- Google Font -->
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-
-
+      <!-- Google Font -->
+      <link rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
 
 
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <!-- Bootstrap 3.3.2 -->
-    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <!-- Font Awesome Icons -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <!-- Ionicons -->
-    <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
-    <!-- fullCalendar 2.2.5-->
-    <link href="../../plugins/fullcalendar/fullcalendar.min.css" rel="stylesheet" type="text/css" />
-    <link href="../../plugins/fullcalendar/fullcalendar.print.css" rel="stylesheet" type="text/css" media='print' />
-    <!-- daterange picker -->
-    <link href="../../plugins/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
-    <!-- iCheck for checkboxes and radio inputs -->
-    <link href="../../plugins/iCheck/all.css" rel="stylesheet" type="text/css" />
-    <!-- Bootstrap Color Picker -->
-    <link href="../../plugins/colorpicker/bootstrap-colorpicker.min.css" rel="stylesheet"/>
-    <!-- Bootstrap time Picker -->
-    <link href="../../plugins/timepicker/bootstrap-timepicker.min.css" rel="stylesheet"/>
-    <!-- Theme style -->
-    <link href="../dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-         folder instead of downloading all of them to reduce the load. -->
-    <link href="../dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
-    <!-- Theme style -->
-    <link href="../../plugins/iCheck/all.css" rel="stylesheet" type="text/css" />
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
+
+      <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+      <!-- Bootstrap 3.3.2 -->
+      <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+      <!-- Font Awesome Icons -->
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+      <!-- Ionicons -->
+      <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+      <!-- fullCalendar 2.2.5-->
+      <link href="../../plugins/fullcalendar/fullcalendar.min.css" rel="stylesheet" type="text/css" />
+      <link href="../../plugins/fullcalendar/fullcalendar.print.css" rel="stylesheet" type="text/css" media='print' />
+      <!-- daterange picker -->
+      <link href="../../plugins/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+      <!-- iCheck for checkboxes and radio inputs -->
+      <link href="../../plugins/iCheck/all.css" rel="stylesheet" type="text/css" />
+      <!-- Bootstrap Color Picker -->
+      <link href="../../plugins/colorpicker/bootstrap-colorpicker.min.css" rel="stylesheet"/>
+      <!-- Bootstrap time Picker -->
+      <link href="../../plugins/timepicker/bootstrap-timepicker.min.css" rel="stylesheet"/>
+      <!-- Theme style -->
+      <link href="../dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+      <!-- AdminLTE Skins. Choose a skin from the css/skins
+            folder instead of downloading all of them to reduce the load. -->
+      <link href="../dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
+      <!-- Theme style -->
+      <link href="../../plugins/iCheck/all.css" rel="stylesheet" type="text/css" />
+
+      <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+      <![endif]-->
+
+      <style>
+         .error {color: #FF0000;}
+      </style>
 </head>
--- admin/Employee/update.php
+++ admin/Employee/update.php
@@ -69,8 +69,8 @@
                                     </div>
                                     <div ><!--hello-->
                                         <div class="col-md-7">
-                                            <div class="form-group">
-                                                <input name="cperson">
+                                            <div  class="form-group">
+                                                <input onkeyup="check_v_mail(email)" id='email' name="cperson">
                                             </div>
                                             <div class="form-group">
                                                 <input name = "address">
@@ -108,52 +108,56 @@
 <?php include_once("employeeComman/footer.php");?>
 
 <?php
+    if(isset($_POST['submit'])) {
+        $currentDir = getcwd();
+        $uploadDirectory = "/employeeComman/upload/";
 
-if(isset($_POST['submit'])) {
-    $currentDir = getcwd();
-    $uploadDirectory = "/employeeComman/upload/";
+        $errors = []; // Store all foreseen and unforseen errors here
 
-    $errors = []; // Store all foreseen and unforseen errors here
+        $fileExtensions = ['jpeg', 'jpg', 'png']; // Get all the file extensions
 
-    $fileExtensions = ['jpeg', 'jpg', 'png']; // Get all the file extensions
+        $fileName = $_FILES['myfile']['name'];
+        $fileSize = $_FILES['myfile']['size'];
+        $fileTmpName = $_FILES['myfile']['tmp_name'];
+        $fileType = $_FILES['myfile']['type'];
+        $fileExtension = strtolower(end(explode('.', $fileName)));
 
-    $fileName = $_FILES['myfile']['name'];
-    $fileSize = $_FILES['myfile']['size'];
-    $fileTmpName = $_FILES['myfile']['tmp_name'];
-    $fileType = $_FILES['myfile']['type'];
-    $fileExtension = strtolower(end(explode('.', $fileName)));
+        $uploadPath = $currentDir . basename($fileName);
 
-    $uploadPath = $currentDir . basename($fileName);
+        if (isset($_POST['submit'])) {
 
-    if (isset($_POST['submit'])) {
+            if (!in_array($fileExtension, $fileExtensions)) {
+                $errors[] = "This file extension is not allowed. Please upload a JPEG or PNG file";
+            }
 
-        if (!in_array($fileExtension, $fileExtensions)) {
-            $errors[] = "This file extension is not allowed. Please upload a JPEG or PNG file";
-        }
+            if ($fileSize > 2000000) {
+                $errors[] = "This file is more than 2MB. Sorry, it has to be less than or equal to 2MB";
+            }
 
-        if ($fileSize > 2000000) {
-            $errors[] = "This file is more than 2MB. Sorry, it has to be less than or equal to 2MB";
-        }
+            if (empty($errors)) {
+                $didUpload = move_uploaded_file($fileTmpName, 'employeeComman/upload/'.$fileName);
 
-        if (empty($errors)) {
-            $didUpload = move_uploaded_file($fileTmpName, 'employeeComman/upload/'.$fileName);
-
-            if ($didUpload) {
-                echo '<script language="javascript" >';
-                echo 'alert("image successfully uploaded")';
-                echo '</script>';
-                // echo "The file " . basename($fileName) . " has been uploaded";
+                if ($didUpload) {
+                    echo '<script language="javascript" >';
+                    echo 'alert("image successfully uploaded")';
+                    echo '</script>';
+                    // echo "The file " . basename($fileName) . " has been uploaded";
+                } else {
+                    echo "An error occurred somewhere. Try again or contact the admin";
+                }
             } else {
-                echo "An error occurred somewhere. Try again or contact the admin";
-            }
-        } else {
-            foreach ($errors as $error) {
-                echo $error . "These are the errors" . "\n";
+                foreach ($errors as $error) {
+                    echo $error . "These are the errors" . "\n";
+                }
             }
         }
-    }
 
-}
+    }
 ?>
+
+
+
+
+
 
 
